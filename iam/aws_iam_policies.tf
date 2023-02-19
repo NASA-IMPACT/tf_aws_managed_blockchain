70d707b9d60a02b29eaa4c17cfc0be70a80db33b
Abdel Marouane <abdelmarouane@abdel22-mbp.attlocal.net>
2023-02-18 22:18:03 -0600

    Adding gitignore

---
+++ iam/aws_iam_policies.tf
@@ -0,0 +1,34 @@
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# MWAA Role
+# ---------------------------------------------------------------------------------------------------------------------
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+
+  }
+}
+#tfsec:ignore:AWS099
+data "aws_iam_policy_document" "ec2_policies" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "ecr:*",
+      "s3:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
