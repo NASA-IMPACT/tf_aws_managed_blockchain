d0152bb7dbbc5bcd84b7d2b367f5ed11e2e822db
Abdel Marouane <abdelmarouane@abdel22-mbp.local>
2023-02-28 11:12:19 -0600

    Fixing the release problem

--- create_release.sh
+++ create_release.sh
@@ -22,7 +22,6 @@ function create_zip_file() {
   mkdir -p ${BUILD_DIR} ${DESTINATION_DIR}
   cp -r blockchain elb vpc ec2 iam sg ${BUILD_DIR}
   cp *tf ${BUILD_DIR}
-  cp *py ${BUILD_DIR}
   cd ${BUILD_DIR}
   zip -r9 ${RELEASE_NAME}.zip .
   mv ${RELEASE_NAME}.zip ${DESTINATION_DIR}/.
